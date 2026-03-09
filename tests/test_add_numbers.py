b4f02c445046b819b014c87f13494bcbe7c0ce8b
Cristina Mitrea <mitrea@umich.edu>
2026-03-09 10:46:49 -0400

    Updated test to add () to the function definition of test_multiplication

--- tests/test_add_numbers.py
+++ tests/test_add_numbers.py
@@ -6,7 +6,7 @@ from cm_pkg import module1
 def test_addn_pos():
 	assert module1.add_numbers(2,5) == 7, "Adding 2 and 5 should result in 7"
 
-def test_multiplication:
+def test_multiplication():
 	assert module1.multiply_numbers(2,5) == 10, "Multiplying 2 and 5 should result in 10"
 
 
